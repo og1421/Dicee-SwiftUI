3bbd8c1199041cc9dbce67e584ca3b26d01b0a79
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-07-07 11:46:24 -0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Orlando Moraes Martins on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
